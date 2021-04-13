d6081059bddebc3b418a9d91d1965aa7ebbe9dc8
Isaac Larbi <isaac.larbi@access89.com>
2021-04-13 11:27:44 +0000

    added new doctype

---
+++ momo/momo/doctype/paystack_settings/paystack_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Isaac larbi and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Paystack Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
