d4b1d7cad0f0bbeaa7c7ecaf3250e8e375b1e782
Isaac Larbi <isaac.larbi@access89.com>
2021-04-14 20:20:45 +0000

    Update paystack_settings.py

--- momo/momo/doctype/paystack_settings/paystack_settings.py
+++ momo/momo/doctype/paystack_settings/paystack_settings.py
@@ -88,7 +88,8 @@ def verify_payment():
     if(frappe.request and frappe.request.data):
         res = json.loads(frappe.request.data)
         if(res["event"] == "paymentrequest.success"):
-            return res["data"]["metadata"]["order_id"]
+			frappe.local.response['http_status_code'] = 200
+            # return res["data"]["metadata"]["order_id"]
         else:
             pass
     else:
