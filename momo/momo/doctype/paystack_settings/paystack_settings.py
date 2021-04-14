318835e4dd57e998d50af904c8de8b3275524dd6
Isaac Larbi <isaac.larbi@access89.com>
2021-04-14 14:26:17 +0000

    Update paystack_settings.py

--- momo/momo/doctype/paystack_settings/paystack_settings.py
+++ momo/momo/doctype/paystack_settings/paystack_settings.py
@@ -3,11 +3,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import json
 import frappe
 import random
 from frappe import _
 import string
+import json
 import requests
 from paystack.resource import TransactionResource
 from frappe.integrations.utils import create_payment_gateway
@@ -35,6 +35,7 @@ class PaystackSettings(Document):
 				_('{currency} is not supported by Paystack at the moment.').format(currency))
 
 	def get_payment_url(self, **kwargs):
+		secret_key = self.get_password(fieldname='secret_key', raise_exception=False)
 		amount = kwargs.get('amount') * 100
 		description = kwargs.get('description')
 		slug = kwargs.get('reference_docname')
@@ -45,32 +46,42 @@ class PaystackSettings(Document):
 			'customer_name': kwargs.get('payer_name')
 		}
 
+		url = "https://api.paystack.co/transaction/initialize/"
+		headers = {
+			"Authorization": "Bearer "+secret_key,
+			"Cache-Control": "no-cache",
+			"Content-Type": "application/json"
+		}
 
+		data = {
+			"amount": amount,
+			"currency": "GHS",
+			"email": email,
+			"metadata": metadata
+		}
+
+		r = requests.post(url, data=json.dumps(data), headers=headers)
+		res_json = r.json()
+
+		print(res_json)
+
+		successful = res_json['status'] == True
+		failed = res_json['status'] == False
+
+		if(successful):
+			authorization_url = res_json['data']['authorization_url']
+			return authorization_url
+		elif(failed):
+			frappe.throw("Request failed with message: "+ res_json['message'])
+
+
+		# rand = ''.join([random.choice(
+        #     string.ascii_letters + string.digits) for n in range(16)])
 		# secret_key = self.get_password(fieldname='secret_key', raise_exception=False)
-
-		# url = "https://api.paystack.co/transaction/initialize"
-		# PARAMS = {
-		# 	"address": amount,
-		# 	"email": email
-		# 	"metadata": metadata
-		# }
-
-		# HEADERS = {
-		# 	"Authorization": "Bearer "+secret_key,
-   		# 	"Cache-Control": "no-cache",
-		# }
-
-		# r = requests.post(url, params = PARAMS, headers=HEADERS)
-		# res = r.json()
-		# return res["data"]["authorization_url"]
-
-		rand = ''.join([random.choice(
-            string.ascii_letters + string.digits) for n in range(16)])
-		secret_key = self.get_password(fieldname='secret_key', raise_exception=False)
-		random_ref = rand
-		client = TransactionResource(secret_key, random_ref)
-		response = client.initialize(amount,email)
-		return response['data']['authorization_url']
+		# random_ref = rand
+		# client = TransactionResource(secret_key, random_ref)
+		# response = client.initialize(amount,email)
+		# return response['data']['authorization_url']
 	
 	
 @frappe.whitelist(allow_guest=True)
@@ -90,29 +101,3 @@ def verify_payment():
 		return True
 	else:
 		return False
-		
-@frappe.whitelist(allow_guest=True)
-def initialize_payment():
-	if(frappe.request and frappe.request.data):
-		data = json.loads(frappe.request.data)
-		
-		url = "https://api.paystack.co/transaction/initialize"
-		
-		PARAMS = {
-			"amount": 10000,
-			"email": "isaac.larbi@access89.com"
-		}
-
-		HEADERS = {
-			"Authorization": "Bearer sk_test_263963288e790e94b572398d0ee801a57e0a7b9c",
-			"Cache-Control": "no-cache",
-			"Content-Type": "application/json"
-		}
-
-		r = requests.post(url, params = PARAMS, headers=HEADERS)
-		res = r.json()
-		return res
-	else:
-		return False
-		
-	
