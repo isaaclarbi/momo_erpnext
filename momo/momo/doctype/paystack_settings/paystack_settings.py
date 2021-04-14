479066269cd7114d0696c9da86a97be3cd48f26d
Isaac Larbi <isaac.larbi@access89.com>
2021-04-14 12:52:18 +0000

    Update paystack_settings.py

--- momo/momo/doctype/paystack_settings/paystack_settings.py
+++ momo/momo/doctype/paystack_settings/paystack_settings.py
@@ -74,7 +74,7 @@ class PaystackSettings(Document):
 	
 	
 @frappe.whitelist(allow_guest=True)
-def verify_payment(**args):
+def verify_payment():
 	# args = frappe._dict(args)
 	frappe.log_error("data", "verify payment function called")
 	
