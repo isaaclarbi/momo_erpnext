e1c6eb4ac9f4b8739a63d593fe2b1e06abdadf04
Isaac Larbi <isaac.larbi@access89.com>
2021-04-18 19:12:37 +0000

    Update example.py

--- momo/example.py
+++ momo/example.py
@@ -1,3 +1,16 @@
-url = "https://kellypee.theadvisorylab.net/api/method/momo.momo.doctype.paystack_settings.paystack_settings.verify_payment/ref"
-ref_list = url.split("/")
-print(ref_list)
+import requests
+
+url =  "https://api.paystack.co/transaction/verify/"+"jv5buhzo4d"
+secret_key = "sk_test_263963288e790e94b572398d0ee801a57e0a7b9c"
+headers = {
+        "Authorization": "Bearer "+secret_key
+    }
+r = requests.get(url, headers=headers)
+response = r.json()
+# print(response)
+
+if(response["status"]):
+    if(response["data"]["status"]=="success"):
+        print(response["data"]["metadata"]["order_id"],'order_id verified')
+else:
+    print(response.message or 'Verification call to Paystack Failed', "Verification call to Paystack Failed")
